a1bae96e8e3be237ad20095fd166e58da30e6484
ReshmaVS01 <96647720+ReshmaVS01@users.noreply.github.com>
2023-11-04 18:58:43 +0530

    Create student.js

---
+++ Lab8/student.js
@@ -0,0 +1,15 @@
+const mongoose=require('mongoose')
+const studentsSchema = new mongoose.Schema({ name : {
+        type: String,
+        required: true
+    },
+    branch:{
+        type: String,
+        required: true
+    },
+    dept:{
+        type: String,
+        required: true
+    }
+})
+module.exports = mongoose.model('Student',studentsSchema)
